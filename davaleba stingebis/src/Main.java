7f2129137380ddac123603da89e626aad73a518e
Leqso Giorgashvili <giorgashvililekso@gmail.com>
2025-01-29 19:15:41 +0400

    davaleba

---
+++ davaleba stingebis/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args)
+    {
+//        String stringi =new String("gamar123jobat");
+//        int raod = 0;
+//        for (int i = 0; i < stringi.length(); i++)
+//        {
+//            if(stringi[i] = "0" || stringi[i] = "1" || stringi[i] = "2" || stringi[i] = "3" || stringi[i] = "4" || stringi[i] = "5" || stringi[i] = "6" || stringi[i] = "7" || stringi[i] = "8" || stringi[i] = "9")
+//            {
+//                raod++;
+//            }
+//        }
+//        String text = "gamarjoba! rogorxar? kargad var. okidoki; shrek!";
+//
+//        int sentenceCount = 0;
+//        for (int i = 0; i < text.length(); i++) {
+//            char ch = text.charAt(i);
+//            if (ch == '.' || ch == '?' || ch == ';' || ch == '!') {
+//                sentenceCount++;
+//            }
+//        }
+//
+//        System.out.println(sentenceCount);
+//        String stringi = "xutiitux";
+//        for(int i = 0; i < stringi.length()/2; i++)
+//        {
+//            for (int j = stringi.length()-1; j > stringi.length()/2; j++)
+//            {
+//                if(stringi.charAt(i) != stringi.charAt(j))
+//                {
+//                    System.out.println("araris palindromi");
+//                }
+//            }
+//        }
+//        System.out.println("palindromia");
+//                String s1 = "shrekis viro";
+//                String s2 = "viro";
+//
+//                if (s1.contains(s2)) {
+//                    System.out.println("s2 არის s1-ის ქვესტრიქონი.");
+//                } else {
+//                    System.out.println("s2 არ არის s1-ის ქვესტრიქონი.");
+//                }
+        
+
+    }
+}
